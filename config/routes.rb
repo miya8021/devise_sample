70861c874047af16dba1837c264a34dd09fbf77e
miya <rinn.sasami@gmail.com>
2021-02-08 08:52:49 +0900

    new.html.erb のname をprice へ修正とroutes.rb の users, の後にスペースを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'items#index'
+  root to: "items#index"
   devise_for :users
   resources :items
-  resource :users,only: :show
+  resource :users, only: :show
 end
